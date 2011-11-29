571ea1eba709fdd145cb6dc04e5df410150916fa
xiongchiamiov <xiong.chiamiov@gmail.com>
2011-11-29 10:52:59 -0800

    stub out MahjonggGame and MahjonggGUI

---
+++ mahjongg/MahjonggGUI.java
@@ -0,0 +1,13 @@
+package mahjongg;
+
+import gridgame.GridGame;
+import gridgame.GridGUI;
+
+public class MahjonggGUI extends GridGUI
+{
+    public MahjonggGUI(String name, MahjonggGame game)
+    {
+        super(name, game);
+    }
+}
+
---
+++ mahjongg/MahjonggGame.java
@@ -0,0 +1,39 @@
+package mahjongg;
+
+import gridgame.GridBoard;
+import gridgame.GridGame;
+import gridgame.GridStatus;
+
+public class MahjonggGame extends GridGame
+{
+    protected GridBoard gridBoard;
+    protected GridStatus gridStatus;
+    
+    public GridBoard getBoardToView()
+    {
+        // TODO: Probably supposed to be a copy.
+        return this.gridBoard;
+    }
+    
+    public GridStatus getStatusToView()
+    {
+        // TODO: Probably supposed to be a copy.
+        return this.gridStatus;
+    }
+    
+    public void init()
+    {
+        // TODO
+    }
+    
+    public void makeMove(int row, int col)
+    {
+        // TODO
+    }
+    
+    public void restart()
+    {
+        // TODO
+    }
+}
+
