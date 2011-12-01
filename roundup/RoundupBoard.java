b033cbfd9bd7ef7d2030faf525e4daac51b0776e
xiongchiamiov <xiong.chiamiov@gmail.com>
2011-12-01 14:39:35 -0800

    place robots on board

--- roundup/RoundupBoard.java
+++ roundup/RoundupBoard.java
@@ -13,6 +13,173 @@ import javax.swing.JOptionPane;
 
 public class RoundupBoard extends GridBoard<RoundupTile>
 {
+    private class RobotCoordinate
+    {
+        public int row;
+        public int column;
+        public boolean isRed;
+        
+        public RobotCoordinate(int row, int column)
+        {
+            this(row, column, false);
+        }
+        
+        public RobotCoordinate(int row, int column, boolean isRed)
+        {
+            this.row = row;
+            this.column = column;
+            this.isRed = isRed;
+        }
+    }
+    
+    private RobotCoordinate[][] boards = {
+        {},
+        { // 1
+            new RobotCoordinate(1, 1),
+            new RobotCoordinate(1, 5),
+            new RobotCoordinate(3, 2, true),
+            new RobotCoordinate(3, 4),
+            new RobotCoordinate(5, 1),
+            new RobotCoordinate(5, 5),
+        },
+        { // 2
+            new RobotCoordinate(2, 2, true),
+            new RobotCoordinate(1, 4),
+            new RobotCoordinate(3, 1),
+            new RobotCoordinate(4, 2),
+            new RobotCoordinate(4, 4),
+            new RobotCoordinate(5, 5),
+        },
+        { // 3
+            new RobotCoordinate(1, 1),
+            new RobotCoordinate(2, 1),
+            new RobotCoordinate(3, 1, true),
+            new RobotCoordinate(5, 1),
+            new RobotCoordinate(1, 5),
+            new RobotCoordinate(4, 5),
+        },
+        { // 4
+            new RobotCoordinate(1, 1),
+            new RobotCoordinate(2, 2, true),
+            new RobotCoordinate(3, 1),
+            new RobotCoordinate(3, 5),
+            new RobotCoordinate(5, 1),
+            new RobotCoordinate(5, 4),
+        },
+        { // 5
+            new RobotCoordinate(1, 1),
+            new RobotCoordinate(2, 2),
+            new RobotCoordinate(2, 5),
+            new RobotCoordinate(3, 1),
+            new RobotCoordinate(4, 1, true),
+            new RobotCoordinate(4, 4),
+        },
+        { // 6
+            new RobotCoordinate(2, 1),
+            new RobotCoordinate(2, 2),
+            new RobotCoordinate(1, 3, true),
+            new RobotCoordinate(3, 3),
+            new RobotCoordinate(4, 2),
+            new RobotCoordinate(4, 3),
+        },
+        { // 7
+            new RobotCoordinate(1, 1),
+            new RobotCoordinate(1, 4, true),
+            new RobotCoordinate(3, 1),
+            new RobotCoordinate(3, 3),
+            new RobotCoordinate(3, 4),
+            new RobotCoordinate(4, 4),
+        },
+        { // 8
+            new RobotCoordinate(1, 1),
+            new RobotCoordinate(1, 3, true),
+            new RobotCoordinate(1, 5),
+            new RobotCoordinate(2, 1),
+            new RobotCoordinate(4, 5),
+            new RobotCoordinate(5, 1),
+        },
+        { // 9
+            new RobotCoordinate(1, 1),
+            new RobotCoordinate(1, 5),
+            new RobotCoordinate(2, 1),
+            new RobotCoordinate(3, 3),
+            new RobotCoordinate(4, 1, true),
+            new RobotCoordinate(4, 4),
+        },
+        { // 10
+            new RobotCoordinate(1, 1),
+            new RobotCoordinate(1, 3),
+            new RobotCoordinate(2, 1),
+            new RobotCoordinate(2, 5, true),
+            new RobotCoordinate(3, 1),
+            new RobotCoordinate(5, 4),
+        },
+        { // 11
+            new RobotCoordinate(1, 3),
+            new RobotCoordinate(1, 5),
+            new RobotCoordinate(2, 5, true),
+            new RobotCoordinate(3, 1),
+            new RobotCoordinate(4, 4),
+            new RobotCoordinate(5, 2),
+        },
+        { // 12
+            new RobotCoordinate(1, 1),
+            new RobotCoordinate(1, 5),
+            new RobotCoordinate(2, 1),
+            new RobotCoordinate(2, 3, true),
+            new RobotCoordinate(4, 5),
+            new RobotCoordinate(5, 1),
+        },
+        { // 13
+            new RobotCoordinate(1, 1),
+            new RobotCoordinate(1, 3),
+            new RobotCoordinate(1, 5),
+            new RobotCoordinate(3, 1),
+            new RobotCoordinate(5, 1),
+            new RobotCoordinate(5, 5, true),
+        },
+        { // 14
+            new RobotCoordinate(1, 1, true),
+            new RobotCoordinate(1, 5),
+            new RobotCoordinate(4, 1),
+            new RobotCoordinate(4, 4),
+            new RobotCoordinate(5, 3),
+            new RobotCoordinate(5, 4),
+        },
+        { // 15
+            new RobotCoordinate(1, 3),
+            new RobotCoordinate(1, 5),
+            new RobotCoordinate(2, 1, true),
+            new RobotCoordinate(2, 5),
+            new RobotCoordinate(5, 2),
+            new RobotCoordinate(5, 5),
+        },
+        { // 16
+            new RobotCoordinate(1, 1, true),
+            new RobotCoordinate(2, 5),
+            new RobotCoordinate(4, 1),
+            new RobotCoordinate(5, 1),
+            new RobotCoordinate(5, 4),
+            new RobotCoordinate(5, 5),
+        },
+        { // 17
+            new RobotCoordinate(1, 4),
+            new RobotCoordinate(2, 1, true),
+            new RobotCoordinate(3, 4),
+            new RobotCoordinate(4, 1),
+            new RobotCoordinate(4, 5),
+            new RobotCoordinate(5, 2),
+        },
+        { // 18
+            new RobotCoordinate(1, 1),
+            new RobotCoordinate(1, 5),
+            new RobotCoordinate(2, 1),
+            new RobotCoordinate(4, 3, true),
+            new RobotCoordinate(4, 5),
+            new RobotCoordinate(5, 1),
+        },
+    };
+    
     // Override GridBoard's parent attribute so we don't have to do casts.
     private RoundupGame parent;
 
@@ -43,9 +210,24 @@ public class RoundupBoard extends GridBoard<RoundupTile>
         }
         this.grid[this.kBoardHeight/2][this.kBoardWidth/2].isCenterTile = true;
         
-        // TODO: Place robots.
+        this.placeRobots();
     }
-
+    
+    private void placeRobots()
+    {
+        for (RobotCoordinate robotCoordinate : this.boards[this.parent.getGame()])
+        {
+            if (robotCoordinate.isRed)
+            {
+                this.grid[robotCoordinate.row][robotCoordinate.column].robot = RoundupTile.Robot.red;
+            }
+            else
+            {
+                this.grid[robotCoordinate.row][robotCoordinate.column].robot = RoundupTile.Robot.green;
+            }
+        }
+    }
+    
     protected void clickTile(final int row, final int column)
     {
         // Basic sanity check.
